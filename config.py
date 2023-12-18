806adc0d0ea088ad851693079067671113f2b05e
Andrea <andrea.giordano991@gmail.com>
2023-12-18 11:39:07 +0100

    requirement fix

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-# import os
-from dotenv import load_dotenv
-load_dotenv()
+import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
-# GPT_MODEL = os.getenv('GPT_MODEL', 'gpt-4-1106-preview')
-# BOT_TOKEN = os.getenv('BOT_TOKEN', '')
-# GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID', 0)
+GPT_MODEL = os.getenv('GPT_MODEL', 'gpt-4-1106-preview')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '')
+GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID', 0)
