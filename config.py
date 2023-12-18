e46a9be057621137305d4d3f42acb55033833161
Andrea <andrea.giordano991@gmail.com>
2023-12-18 12:23:44 +0100

    openai key

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 # from dotenv import load_dotenv
 # load_dotenv()
 
-GPT_MODEL = os.getenv('GPT_MODEL', 'gpt-4-1106-preview')
-BOT_TOKEN = os.getenv('BOT_TOKEN', '')
-GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID', 0)
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '')
+GPT_MODEL = os.environ.get('GPT_MODEL', 'gpt-4-1106-preview')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+GROUP_CHAT_ID = os.environ.get('GROUP_CHAT_ID', 0)
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '')
