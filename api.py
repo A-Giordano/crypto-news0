61ba6ef2fa64ecbc360d52a4e6147e7ab9245061
Andrea <andrea.giordano991@gmail.com>
2023-12-18 11:59:13 +0100

    openai key

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from utils import get_summary, send_message, get_transcript
 app = FastAPI()
 
 @app.post("/transcript")
-async def get_transcript(request: Request):
+async def transcript(request: Request):
     # Extract JSON body from the request. Expected format: {"url": "https://www.youtube.com/watch?v=YOUR_VIDEO_ID"}
     data = await request.json()
     video_url = data.get("url")
