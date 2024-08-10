b7c4a0d18d44fc383410838756075c599182c19e
Andrea <andrea.giordano991@gmail.com>
2024-08-10 22:36:55 +0200

    youtube-transcript-api==0.6.2

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from utils import get_transcript
 
 if __name__ == "__main__":
     # live video
-    youtube_transcript = get_transcript("https://www.youtube.com/watch?v=QYUf8XolaNc")
+    youtube_transcript = get_transcript("https://www.youtube.com/watch?v=iDRLGmyNWDU")
 
     # youtube_transcript = get_transcript("https://www.youtube.com/watch?v=8mnnY0LFx_0")
     # youtube_transcript = get_transcript("https://www.youtube.com/watch?v=uARGTP3ehBU")
